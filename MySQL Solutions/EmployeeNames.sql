e9be0e6cba6dab0a4b279c9ccbe16e54f4a28cd4
neroAzsy12 <azael.zamora12@gmail.com>
2021-06-24 23:59:58 -0700

    Add files via upload

---
+++ MySQL Solutions/EmployeeNames.sql
@@ -0,0 +1,5 @@
+-- Write a query that prints a list of employee names from the Employee table in alphabetical order
+-- ASC sorts from least to greatest, DESC sorts from greatest to least
+SELECT name
+FROM Employee
+ORDER BY name ASC;
