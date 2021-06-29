24d7e1431460579c9e8e42258081fb9a36100313
neroAzsy12 <azael.zamora12@gmail.com>
2021-06-28 19:41:00 -0700

    Add files via upload

---
+++ Data Structures/Arrays_DS.cpp
@@ -0,0 +1,35 @@
+// g++ -o filename filename.cpp -std=c++14
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> reverseArray(vector<int> a) {
+    for(int i = 0; i < a.size() / 2; i++) {
+        int tmp = a[i];
+        a[i] = a[a.size() - 1 - i];
+        a[a.size() - 1 - i] = tmp;
+    }
+
+    return a;
+}
+
+int main() {
+    vector<int> test;
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        test.push_back(tmp);
+    }
+
+    vector<int> result = reverseArray(test);
+
+    for(int i = 0; i < result.size(); i++) {
+        printf("%d ", result[i]);
+    }
+    printf("\n");
+}
